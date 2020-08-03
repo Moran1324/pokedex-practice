51700ed43454864b63fb19987652855900e4a6c6
Moran1324 <moran1324@gmail.com>
2020-08-03 18:14:57 +0300

    error try catch no pokemon found

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ const searchButton = document.getElementById("searchButton");
 const result = document.getElementById("results");
 
 const searchPokemon = async (pokemonId = 6) => {
+  try {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   makeDiv(data.name, data.height, data.weight, data.sprites.front_default, data.sprites.back_default);
   searchBar.value = "";
   searchBar.focus();
+  } catch(e) {
+    result.innerHTML = "";
+    result.innerHTML = "Pokemon Not Found";
+    console.log(e);
+  }
   };
-// searchPokemon();
 
 const makeDiv = (name, height, weight, picture, over) => {
   const htmlText = `
@@ -24,7 +29,6 @@ const makeDiv = (name, height, weight, picture, over) => {
   const image = document.getElementById("pokemonPic");
     image.onmouseover= () => image.src=over;
     image.onmouseout= () => image.src=picture;
-
 }
 
 searchButton.addEventListener("click", () =>  searchPokemon(searchBar.value));
