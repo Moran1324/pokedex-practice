c8b38e5ae44907635710a150ca61487cac72fbce
Moran1324 <moran1324@gmail.com>
2020-08-03 14:53:49 +0300

    function working to console on click

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+const searchBar = document.getElementById("search");
+const searchButton = document.getElementById("searchButton");
+const result = document.getElementById("result");
+
 const searchPokemon = async (pokemonId = 3) => {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   console.log(data)
 };
-searchPokemon();
+// searchPokemon();
 
+
+searchButton.addEventListener("click", () => { searchPokemon(searchBar.value);} );
