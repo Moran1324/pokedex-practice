de83984d1ae87d766d12d3343fe1b00529988bfb
Moran1324 <moran1324@gmail.com>
2020-08-03 18:32:17 +0300

    pokemon types

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const result = document.getElementById("results");
 const searchPokemon = async (pokemonId = 6) => {
   try {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  makeDiv(data.name, data.height, data.weight, data.sprites.front_default, data.sprites.back_default);
+  console.log(data);
+  makeDiv(data.name, data.height, data.weight, data.sprites.front_default, data.sprites.back_default, data.types);
   searchBar.value = "";
   searchBar.focus();
   } catch(e) {
@@ -15,12 +16,17 @@ const searchPokemon = async (pokemonId = 6) => {
   }
   };
 
-const makeDiv = (name, height, weight, picture, over) => {
+const makeDiv = (name, height, weight, picture, over, types) => {
+  let pokemonTypes = " ";
+  for (let i = 0; i < types.length; i++) {
+    pokemonTypes += types[i].type.name + " ";
+  }
   const htmlText = `
   <div class="pokemonContainer">
   <div>Name: ${name}</div>
   <div>height: ${height}</div>
   <div>weight: ${weight}</div>
+  <div>types: ${pokemonTypes} </div>
   <div>picture: <img id="pokemonPic" src="${picture}"/></div>
   </div>
   `;
