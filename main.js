1ecbff3f2970e3dc65431f2ac4c256fa361dc2d5
Moran1324 <moran1324@gmail.com>
2020-08-03 16:47:24 +0300

    mouseover fix

--- main.js
+++ main.js
@@ -2,27 +2,29 @@ const searchBar = document.getElementById("search");
 const searchButton = document.getElementById("searchButton");
 const result = document.getElementById("results");
 
-const searchPokemon = async (pokemonId) => {
+const searchPokemon = async (pokemonId = 6) => {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  makeDiv(data.name, data.height, data.weight, data.sprites.front_default    
-    );
-    searchBar.value = "";
-    searchBar.focus();
-
-  console.log(data)
-};
+  makeDiv(data.name, data.height, data.weight, data.sprites.front_default, data.sprites.back_default);
+  searchBar.value = "";
+  searchBar.focus();
+  };
 // searchPokemon();
 
-const makeDiv = (name, height, weight, picture) => {
+const makeDiv = (name, height, weight, picture, over) => {
   const htmlText = `
-    <div class="pokemonContainer">
-      <div>Name: ${name}</div>
-      <div>height: ${height}</div>
-      <div>weight: ${weight}</div>
-      <div>picture: <img src="${picture}" /></div>
-    </div>
+  <div class="pokemonContainer">
+  <div>Name: ${name}</div>
+  <div>height: ${height}</div>
+  <div>weight: ${weight}</div>
+  <div>picture: <img id="pokemonPic" src="${picture}"/></div>
+  </div>
   `;
   result.innerHTML = htmlText;
+  
+  const image = document.getElementById("pokemonPic");
+    image.onmouseover= () => image.src=over;
+    image.onmouseout= () => image.src=picture;
+
 }
 
 searchButton.addEventListener("click", () =>  searchPokemon(searchBar.value));
