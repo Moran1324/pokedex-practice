352d0b578a72f5e340c299a6980a15279a6d5f18
Moran1324 <moran1324@gmail.com>
2020-08-04 19:07:52 +0300

    clear code with comments

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const searchBar = document.getElementById("search");
 const searchButton = document.getElementById("searchButton");
 const result = document.getElementById("results");
 
+//search pokemon by id or name
 const searchPokemon = async (pokemonId = 6) => {
   try {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
@@ -17,10 +18,10 @@ const searchPokemon = async (pokemonId = 6) => {
     console.log(e);
   }
   };
-
+// build the div contains pokemon info
 const makeDiv = (name, height, weight, picture, over, types) => {
   let pokemonTypes = " ";
-  for (let i = 0; i < types.length; i++) {
+  for (let i = 0; i < types.length; i++) { //add pokemon type names to info
     pokemonTypes += `<div class='type' id='${types[i].type.name}'>type ${i + 1}: ${types[i].type.name}</div>`;
   }
   const htmlText = `
@@ -34,17 +35,19 @@ const makeDiv = (name, height, weight, picture, over, types) => {
   `;
   result.innerHTML = htmlText;
   
+  // when mouse hovers image pokemon turns around
   const image = document.getElementById("pokemonPic");
     image.onmouseover= () => image.src=over;
     image.onmouseout= () => image.src=picture;
 }
 
-searchButton.addEventListener("click", () =>  searchPokemon(searchBar.value));
+searchButton.addEventListener("click", () =>  searchPokemon(searchBar.value)); //search button activates pokemon search
 
+// clicking a types creates a list of all pokemon with the same type
 result.addEventListener('click', async (e) => {
   const target = e.target;
   if (target.className === 'type') {
-    let pokemons = document.createElement('div');
+    let pokemons = document.createElement('div'); // element contains all the clicked type's pokemons
     for (let i = 1; i <= 807; i++){
       const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${i}`);
       console.log(i);
@@ -57,8 +60,10 @@ result.addEventListener('click', async (e) => {
           }
       }                 
   }
-result.appendChild(pokemons);
+result.appendChild(pokemons); // clicked type's pokemons list appears at once
 }
+
+// when a pokemon from the clicked type's list is clicked, it appears as the searched pokemon
 console.log(target.className);
 if (target.className === 'pokemons') {
   searchPokemon(target.innerHTML);
