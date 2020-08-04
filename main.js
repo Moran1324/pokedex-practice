0057c8c8a1d5925f790d77c0c41d022c5fef3d4c
Moran1324 <moran1324@gmail.com>
2020-08-04 18:03:24 +0300

    finished tasks

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const searchPokemon = async (pokemonId = 6) => {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   console.log(data);
   makeDiv(data.name, data.height, data.weight, data.sprites.front_default, data.sprites.back_default, data.types);
+
+
   searchBar.value = "";
   searchBar.focus();
   } catch(e) {
@@ -19,14 +21,14 @@ const searchPokemon = async (pokemonId = 6) => {
 const makeDiv = (name, height, weight, picture, over, types) => {
   let pokemonTypes = " ";
   for (let i = 0; i < types.length; i++) {
-    pokemonTypes += types[i].type.name + " ";
+    pokemonTypes += `<div class='type' id='${types[i].type.name}'>type ${i + 1}: ${types[i].type.name}</div>`;
   }
   const htmlText = `
   <div class="pokemonContainer">
   <div>Name: ${name}</div>
   <div>height: ${height}</div>
   <div>weight: ${weight}</div>
-  <div>types: ${pokemonTypes} </div>
+  ${pokemonTypes}
   <div>picture: <img id="pokemonPic" src="${picture}"/></div>
   </div>
   `;
@@ -38,3 +40,27 @@ const makeDiv = (name, height, weight, picture, over, types) => {
 }
 
 searchButton.addEventListener("click", () =>  searchPokemon(searchBar.value));
+
+result.addEventListener('click', async (e) => {
+  const target = e.target;
+  if (target.className === 'type') {
+    let pokemons = document.createElement('div');
+    for (let i = 1; i <= 807; i++){
+      const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${i}`);
+      console.log(i);
+      for(let j = 0; j < data.types.length; j++) {
+          if (data.types[j].type.name == target.id) {
+              pokemon = document.createElement('p');
+              pokemon.innerHTML = data.name;
+              pokemon.className = 'pokemons';
+              pokemons.appendChild(pokemon);
+          }
+      }                 
+  }
+result.appendChild(pokemons);
+}
+console.log(target.className);
+if (target.className === 'pokemons') {
+  searchPokemon(target.innerHTML);
+}
+})
